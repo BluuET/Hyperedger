4d33bd150e70c1da1741fed73a576e75523dac82
tittuvarghese <tittuhpd@gmail.com>
2018-07-23 16:41:38 +0530

    Minor fixes

--- blockchain/query.go
+++ blockchain/query.go
@@ -22,22 +22,23 @@ func (setup *FabricSetup) QueryAll() (string, error) {
 	}
 
 	return string(response.Payload), nil
-	
-	
+}
+
+
 	// QueryOne query the chaincode to get the record of a specific Key
 	func (setup *FabricSetup) QueryOne(value string) (string, error) {
-	
+
 		// Prepare arguments
 		var args []string
 		args = append(args, "invoke")
 		args = append(args, "queryone")
 		args = append(args, value)
-	
+
 		response, err := setup.client.Query(chclient.Request{ChaincodeID: setup.ChainCodeID, Fcn: args[0], Args: [][]byte{[]byte(args[1]), []byte(args[2])}})
 		if err != nil {
 			return "", fmt.Errorf("failed to query: %v", err)
 		}
-	
+
 		return string(response.Payload), nil
 	}
 }
